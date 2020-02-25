74db92853541fe48e45bb183b925871137bcdf24
Kqzhang <pigzkqlover@163.com>
2020-02-25 18:44:29 +0800

    数列分段

---
+++ Leetcode/2015-09-1.cpp
@@ -0,0 +1,35 @@
+/*
+	input:
+	8
+	8 8 8 0 12 12 8 0
+	output:
+	5
+*/
+
+//ÊýÁÐ·Ö¶Î
+//http://118.190.20.162/view.page?gpid=T32
+
+/*
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int n, m, count = 1, last;
+	cin >> n;
+	cin >> last;
+	for (int i = 1; i < n; i++){
+		cin >> m;
+		if (last == m)
+			continue;
+		else{
+			count++;
+			last = m;
+		}
+	}
+
+	cout << count << endl;
+
+	return 0;
+}
+*/
